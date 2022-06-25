a518840d9b353c2999534565788389c44f94aa2f
Takuya Azumaguchi <catchballer@gmail.com>
2022-06-25 21:13:59 +0900

    Add util

---
+++ src/core/include/util.h
@@ -0,0 +1,10 @@
+//
+// Created by 東口拓也 on 2022/06/25.
+//
+
+#ifndef ROLL_SHOOTING_UTIL_H
+#define ROLL_SHOOTING_UTIL_H
+
+void swap(float *a, float *b);
+
+#endif //ROLL_SHOOTING_UTIL_H
---
+++ src/core/util.c
@@ -0,0 +1,10 @@
+//
+// Created by 東口拓也 on 2022/06/25.
+//
+
+void swap(float *a, float *b)
+{
+    float tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
